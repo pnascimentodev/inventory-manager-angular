4087f8c012632fccb83afce517a88236c423c424
Priscila Nascimento <pnmelo2808@gmail.com>
2025-05-21 19:07:06 -0300

    Fix: Adjustment loginpage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
