50d6ca9e23c11755009cf5477496611b8ca1fa4b
Priscila Nascimento <pnmelo2808@gmail.com>
2025-05-21 22:18:17 -0300

    Feat: Dashboard create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
@@ -14,5 +15,9 @@ export const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
     }
 ];
